35c83d5ae6e21b92603b9a5049b7c50c94e30dba
meshcheryagin_sergey <s.meshcheryagin@hostco.ru>
2022-03-10 21:48:44 +0500

    Rework cshtml and work with controllers

--- EMAProject/Startup.cs
+++ EMAProject/Startup.cs
@@ -20,20 +20,14 @@ namespace EMAProject
             .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Latest)
             .AddSessionStateTempDataProvider();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
             app.UseRouting();
             app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("default","{controller=Home}/{action = Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
